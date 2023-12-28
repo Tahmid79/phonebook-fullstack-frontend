3ee4bd266913b647bb0c8549f653fb486c856314
Tahmid <tahmidfarhan12@gmail.com>
2023-12-29 01:58:52 +0600

    reset token if refresh token is not valid

--- src/app/refresh/refresh.interceptor.ts
+++ src/app/refresh/refresh.interceptor.ts
@@ -35,7 +35,8 @@ export class RefreshInterceptor implements HttpInterceptor {
 
               return next.handle(authReq);
             }
-
+            this.resetTokens();
+            
             return throwError(error);
           })
          ) 
